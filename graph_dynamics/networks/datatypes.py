5e110a99d2c4089740f87b755e18c820f15b9150
Ernane Luis <ernane.luis@gmail.com>
2017-07-20 11:08:18 +0200

    Revert "removed abstract annotation from Graph method because it forced a not reusable code. so for  simplification inherited class from Graph do not need necessary implement such methods unless is necessary, if so, just overwrite it."
    This reverts commit 1f749d1089716c99f1e10183def95224825c7d89.

--- graph_dynamics/networks/datatypes.py
+++ graph_dynamics/networks/datatypes.py
@@ -15,7 +15,6 @@ from matplotlib import pyplot as plt
 from scipy.integrate import quadrature
 from scipy.stats import poisson, gamma
 from abc import ABCMeta, abstractmethod
-from scipy.stats import pareto, norm, bernoulli
 from _pyio import __metaclass__
 
 from graph_dynamics.random_measures.process import GammaProcess
@@ -30,33 +29,37 @@ class Graph(object):##<------------------
     __metaclass__ = ABCMeta
 
     def __init__(self,name_string,identifier_string,graph_state):
-        self.name_string        = name_string
-        self.identifier_string  = identifier_string
-        self.graph_state        = graph_state
-        self.networkx_graph     = nx.Graph()
-
+        self.name_string = name_string
+        self.identifier_string = identifier_string
+        self.graph_state = graph_state
+        
+    @abstractmethod
     def get_graph_state(self):
         """
-                This function should return a json object with all
-                parameters required to initialize such a graph
+        This function should return a json object with all 
+        parameters required to initialize such a graph 
         """
-        return self.graph_state
-
+        raise NotImplemented()
+        
+    @abstractmethod        
     def get_networkx(self):
-        return self.networkx_graph
-
+        raise NotImplemented()
+        
+    @abstractmethod      
     def get_adjancency_matrix(self):
-        return nx.adj_matrix(self.networkx_graph)
+        raise NotImplemented()
 
+    @abstractmethod        
     def get_edge_list(self):
-        return self.networkx_graph.edges()
-
+        raise NotImplemented()
+ 
+    @abstractmethod       
     def get_number_of_edges(self):
-        return self.networkx_graph.number_of_edges()
+        raise NotImplemented()
 
+    @abstractmethod        
     def get_number_of_nodes(self):
-        return self.networkx_graph.number_of_nodes()
-
+        raise NotImplemented()    
 
 class VanillaGraph(Graph):
     """
@@ -78,7 +81,27 @@ class VanillaGraph(Graph):
             
         Graph.__init__(self,self.name_string,self.identifier_string,self.graph_state)
 
+    def get_graph_state(self):
+        """
+        This function should return a json object with all 
+        parameters required to initialize such a graph 
+        """
+        return self.graph_state
+            
+    def get_networkx(self):
+        return self.networkx_graph
+          
+    def get_adjancency_matrix(self):
+        return nx.adjacency_matrix(self.networkx_graph)
 
+    def get_edge_list(self):
+        return self.networkx_graph.edge
+     
+    def get_number_of_edges(self):
+        return self.networkx_graph.number_of_edges()
+     
+    def get_number_of_nodes(self):
+        return self.networkx_graph.number_of_nodes()     
     
 class CommunityGraphs(Graph):
     __metaclass__ = ABCMeta
@@ -86,8 +109,8 @@ class CommunityGraphs(Graph):
     def __init__(self,name_string,identifier_string):
         self.name_string = name_string
         self.identifier_string = identifier_string
-
-    @abstractmethod
+        
+    @abstractmethod        
     def get_nodes(self):
         raise NotImplemented()
     
@@ -106,11 +129,11 @@ class BayesianGraph(Graph):
         self.name_string = name_string
         self.identifier_string = identifier_string
         Graph.__init__(self,name_string, identifier_string, graph_state)
-
+        
     @abstractmethod
     def inferMeasures(self):
         raise NotImplemented()
-
+    
     @abstractmethod
     def generateNetwork(self):
         raise NotImplemented()
@@ -216,6 +239,26 @@ class FiniteProcessGraphs(BayesianGraph):
     def inferMeasures(self):
         return None
 
+    #===============================
+    # LIBRARY GRAPH REQUIREMENTS
+    #===============================
+    def get_networkx(self):
+        return self.networkx_graph
+
+    def get_adjancency_matrix(self):
+        return nx.adj_matrix(self.networkx_graph) 
+    
+    def get_edge_list(self):
+        return self.networkx_graph.edges()
+        
+    def get_number_of_edges(self):
+        return self.networkx_graph.number_of_edges()
+        
+    def get_number_of_nodes(self):
+        return self.networkx_graph.number_of_nodes()
+    
+    def get_graph_state(self):
+        return None
      
 #==============================================================
 #                           CARON FOX 
@@ -287,7 +330,9 @@ class CaronFoxGraphs(BayesianGraph):
     def set_C(self,C):
         self.C = C
         self.graph_state["C"] = C
-
+        
+    def get_graph_state(self):
+        return self.graph_state
         
     def generateNetwork(self,sigma_increment=0.,tau_increment=0.,table_and_costumers=None):
         """
@@ -319,7 +364,24 @@ class CaronFoxGraphs(BayesianGraph):
     #===============================
     def inferMeasures(self):
         return None
+    
+    #===============================
+    # LIBRARY GRAPH REQUIREMENTS
+    #===============================
+    def get_networkx(self):
+        return self.networkx_graph
 
+    def get_adjancency_matrix(self):
+        return nx.adj_matrix(self.networkx_graph) 
+    
+    def get_edge_list(self):
+        return self.networkx_graph.edges()
+        
+    def get_number_of_edges(self):
+        return self.networkx_graph.number_of_edges()
+        
+    def get_number_of_nodes(self):
+        return self.networkx_graph.number_of_nodes()
 
 
 #==============================================================
@@ -367,247 +429,6 @@ class CryptocurrencyGraphs(FromFileGraph):
         for node_index, node_d in self.node_data.iteritems():
             self.ecosystem_crash[node_d['cryptocurrency']] = node_d['AV_DATEOFCRASH']
         return self.ecosystem_crash
-
-# ==============================================================
-#                           ACTIVITY DRIVEN
-# ==============================================================
-
-class ActivityDrivenGraph(Graph):
-
-
-    def __init__(self, identifier_string,
-                 graph_state=None,
-                 numberOfNodes=1,
-                 activity_gamma=2.8,
-                 rescaling_factor=None,
-                 threshold_min=0.001,
-                 delta_t=1):
-        """
-          Constructor
-
-          Parameters
-
-            int     numberOfNodes:     to specify number of nodes of the graph
-            float   activity_gamma:    alpha value of pareto distribution formula
-            int     rescaling_factor:  number which scales the activity potential
-            float   threshold_min:     set the min values of the activity potential from the pareto distribution
-            int     delta_t:           time gap
-
-        """
-
-        ######################### config variables #########################
-        # initialize with parameters
-        Graph.__init__(self, "ActivityDrivenGraph", identifier_string, copy.copy(graph_state))
-
-
-        # creating graph
-        self.networkx_graph     = nx.Graph()
-        self.activity_potential = self.__calculateActivityPotential(activity_gamma, threshold_min, numberOfNodes)
-        self.networkx_graph.add_nodes_from(list(xrange(numberOfNodes)))  # creating list of nodes with index from 0 de N-1  adding to the graph
-
-        ######################### end config variables #########################
-        self.hasGraphAmount = False
-
-        ######################### initializing graph  #########################
-        # run over all nodes to set initial attributes
-        for n in self.networkx_graph.nodes():
-            ## what is the purpose of rescaling factor?
-            # ai = xi*n => probability per unit time to create new interactions with other nodes
-            # activity_firing_rate is an probability number than [0,1]
-            self.networkx_graph.node[n]['activity_firing_rate'] = self.activity_potential[n] * rescaling_factor
-
-            # With probability ai*delta_t each vertex i becomes active and generates m links that are connected to m other randomly selected vertices
-            self.networkx_graph.node[n]['activity_probability'] = self.networkx_graph.node[n]['activity_firing_rate'] * delta_t
-
-
-
-    # graph paths should return a list of graphs objects
-            ######################### PRIVATE  METHODS  #########################
-
-    def __calculateActivityPotential(self, activity_gamma, threshold_min, numberOfNodes):
-        ## calculating the activity potential following pareto distribution
-        X = pareto.rvs(activity_gamma, loc=threshold_min, size=numberOfNodes)  # get N samples from  pareto distribution
-        X = X / max(X)  # every one smaller than one
-        return np.take(X, np.where(X > threshold_min)[0])  # using the thershold
-
-    ######################### PUBLIC  METHODS  #########################
-
-    def get_active_nodes(self):
-        # return the list of choosed active nodes
-        return [n for n in self.networkx_graph.nodes() if self.networkx_graph.node[n]['type'] == 1]
-
-    def number_of_nodes(self):
-        return self.networkx_graph.number_of_nodes()
-
-    def get_activity_firing_rate(self, node):
-        return self.networkx_graph.node[node]['activity_firing_rate']
-
-    def get_node_type(self, node):
-        return self.networkx_graph.node[node]['type']
-
-    def set_node_type(self, node):
-        ## assign a node attribute nonactive or active
-        # is sample the activity once and do the bernoully sample at each time step
-        activity_firing_rate = self.get_activity_firing_rate(node)
-        if (activity_firing_rate > 1):
-            activity_firing_rate = 1 / activity_firing_rate
-        # set if a node is active or not
-        self.networkx_graph.node[node]['type'] = bernoulli.rvs(activity_firing_rate)
-
-# ==============================================================
-#                           PERRA
-# ==============================================================
-
-class PerraGraph(ActivityDrivenGraph):
-
-    def __init__(self, identifier_string,
-                 graph_state,
-                 numberOfNodes,
-                 activity_gamma,
-                 rescaling_factor,
-                 threshold_min,
-                 delta_t,
-                 number_walkers):
-        """
-          Constructor
-
-          Parameters
-
-            int     numberOfNodes:     to specify number of nodes of the graph
-            float   activity_gamma:    alpha value of pareto distribution formula
-            int     rescaling_factor:  number which scales the activity potential
-            float   threshold_min:     set the min values of the activity potential from the pareto distribution
-            int     delta_t:           time gap
-
-        """
-
-
-        ######################### config variables #########################
-        ActivityDrivenGraph.__init__(self, identifier_string, graph_state, numberOfNodes, activity_gamma, rescaling_factor, threshold_min, delta_t)
-
-        ######################### initializing graph  #########################
-        # run over all nodes to set initial attributes
-        for n in self.networkx_graph.nodes():
-            self.networkx_graph.node[n]['walker'] = 0
-
-        self.init_walkers(number_walkers)
-
-    # ===============================================
-    # WALKERS FUNCTIONS
-    # ===============================================
-
-    def init_walkers(self, number_walkers):
-        # create W walkers on those nodes
-        self.number_walkers = number_walkers
-        generate_walkers = np.random.choice(self.networkx_graph.nodes(), size=number_walkers, replace=False)
-        for node in generate_walkers:
-            self.add_walker(node)
-
-    def transfer_walker(self, _from, _to):
-        self.remove_walker(_from)
-        self.add_walker(_to)
-
-    def add_walker(self, node):
-        self.networkx_graph.node[node]['walker'] += 1
-
-    def remove_walker(self, node):
-        self.networkx_graph.node[node]['walker'] -= 1
-
-    # return list of nodes which has walkers
-    def get_walkers(self):
-        out = []
-        for node in self.networkx_graph.nodes():
-            if self.networkx_graph.node[node]['walker'] > 0:
-                out += [node]
-
-        return out
-
-# ==============================================================
-#                 Bitcoin Transactions Graph
-# ==============================================================
-
-class TxGraph(PerraGraph):
-
-    def __init__(self, identifier_string,
-                 graph_state,
-                 numberOfNodes,
-                 activity_gamma,
-                 rescaling_factor,
-                 threshold_min,
-                 delta_t,
-                 number_walkers,
-                 amount_pareto_gama,
-                 amount_threshold):
-        """
-          Constructor
-
-          Parameters
-
-            int     numberOfNodes:     to specify number of nodes of the graph
-            float   activity_gamma:    alpha value of pareto distribution formula
-            int     rescaling_factor:  number which scales the activity potential
-            float   threshold_min:     set the min values of the activity potential from the pareto distribution
-            int     delta_t:           time gap
-
-        """
-        PerraGraph.__init__(self, identifier_string,
-                            graph_state,
-                            numberOfNodes,
-                            activity_gamma,
-                            rescaling_factor,
-                            threshold_min,
-                            delta_t,
-                            number_walkers)
-        ######################### config variables #########################
-
-        self.init_amount(amount_pareto_gama, amount_threshold)
-
-    ######################### PRIVATE  METHODS  #########################
-
-
-    # ===============================================
-    # WALKERS FUNCTIONS
-    # ===============================================
-
-    def transfer_walker(self, _from, _to):
-        self.remove_walker(_from)
-        self.add_walker(_to)
-
-        # right now when a walker move, he moves all the money
-        # TODO: add a probability to move % of the amount
-        amount_to_move = self.get_amount(_from)
-        self.remove_amount(_from, amount_to_move)
-        self.add_amount(_to, amount_to_move)
-
-    # ===============================================
-    # AMOUNT FUNCTIONS
-    # ===============================================
-
-    # setting the node the initial amount of wealth
-    def init_amount(self, amount_pareto_gama, amount_threshold):
-        self.hasGraphAmount = True
-
-        # calculate the wealth distribution following pareto law
-        A = pareto.rvs(amount_pareto_gama, loc=amount_threshold, scale=1,
-                       size=self.networkx_graph.number_of_nodes())
-        self.amount = A / max(A)
-
-        # run over all nodes to set initial attributes
-        for n in self.networkx_graph.nodes():
-            # setting the node the initial amount of wealth
-            self.networkx_graph.node[n]['amount'] = self.amount[n]
-
-    def get_amount(self, node):
-        return self.networkx_graph.node[node]['amount']
-
-    def add_amount(self, node, amount):
-        self.networkx_graph.node[node]['amount'] += amount
-
-    def remove_amount(self, node, amount):
-        self.networkx_graph.node[node]['amount'] -= amount
-
-    def get_nodes_amounts(self):
-        return [self.networkx_graph.node[node]['amount'] for node in self.networkx_graph.nodes()]
 
 #======================================================
 
